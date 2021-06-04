449f30f090b2b597339e8d31a5a3f43ffbb894c2
Jerome Viray <jeromeviray4@gmail.com>
2021-06-05 02:23:08 +0800

    Cart Management

---
+++ src/main/java/com/project/inventory/cart/model/ShoppingCart.java
@@ -0,0 +1,10 @@
+package com.project.inventory.cart.model;
+
+import com.project.inventory.product.model.Product;
+
+import java.util.List;
+
+public class ShoppingCart {
+    private int id;
+    private List<Product> product;
+}
