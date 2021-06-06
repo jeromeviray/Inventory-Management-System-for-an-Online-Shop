7b7cce3e31dad84878e51a6d5198ea9be4739b29
Jerome Viray <jeromeviray4@gmail.com>
2021-06-07 00:10:11 +0800

    Sql script been modified

---
+++ src/main/java/com/project/inventory/cart/model/Cart.java
@@ -0,0 +1,8 @@
+package com.project.inventory.cart.model;
+
+import com.project.inventory.permission.model.Account;
+
+public class ShoppingCart {
+    private int id;
+    private Account account;
+}
--- src/main/resources/Script-SQL.sql
+++ src/main/resources/Script-SQL.sql
@@ -14,3 +14,45 @@
 --     stock int(7) not null default 1,
 --     product_id int(7) not null
 -- );
+
+-- create table imsos.account(
+-- 	account_id int primary key auto_increment,
+--     first_name varchar(15) not null,
+--     last_name varchar(15) not null,
+--     created_at datetime not null default current_timestamp,
+--     updated_at datetime not null default current_timestamp on update current_timestamp,
+--     is_deleted tinyint not null default 0
+-- );
+
+create table imsos.cart(
+	cart_id int primary key auto_increment,
+    account_id int not null,
+    order_item_id int not null
+);
+
+create table imsos.order_item(
+	order_item_id int primary key auto_increment,
+    quantity int not null default 1,
+    total_amount double not null,
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp,
+    is_removed tinyint not null default 0,
+    product_id int not null
+);
+
+create table imsos.cart_item(
+	cart_item_id int primary key auto_increment,
+    quantity int not null default 1,
+    amount double not null,
+    created_at datetime default current_timestamp,
+    remove_at datetime,
+    product_id int not null,
+    cart_id int not null
+);
+
+
+
+
+-- insert into imsos.account (first_name, last_name) values ("jerome","viray");
+
+-- update imsos.account set last_name = "seccion" where account_id = 1;
