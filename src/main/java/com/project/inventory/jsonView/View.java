05e7642d80399a19731e89e894bee5db5d1c5181
@jeromeviray <jeromeviray4@gmail.com>
2021-06-07 17:58:31 +0800

    Json View been added

---
+++ src/main/java/com/project/inventory/jsonView/View.java
@@ -0,0 +1,16 @@
+package com.project.inventory.jsonView;
+
+public class View {
+    public static interface ProductView{
+        public static interface CartView{
+
+        }
+        public static interface ProductsView{
+
+        }
+
+        public static interface CartItemView{
+
+        }
+    }
+}
