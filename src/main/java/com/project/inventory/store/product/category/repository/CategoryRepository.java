d8c916225fb9eef988adc0d69b2f55266933b237
Jerome Viray <jeromeviray4@gmail.com>
2021-09-27 21:18:56 +0800

    feat: soft delete category implemented'

--- src/main/java/com/project/inventory/store/product/category/repository/CategoryRepository.java
+++ src/main/java/com/project/inventory/store/product/category/repository/CategoryRepository.java
@@ -8,10 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    @Query(value = "SELECT category.id, category.category_name as categoryName, count(p.id) totalProducts " +
+    @Query(value = "SELECT category.id, category.category_name as categoryName, category.is_deleted as deleted, count(product.id) totalProducts " +
             "FROM product_categories as category " +
-            "LEFT JOIN products as p " +
-            "ON category.id = p.id " +
+            "LEFT JOIN products as product " +
+            "ON category.id = product.id " +
+            "WHERE category.is_deleted = 0 " +
             "GROUP BY category.id", nativeQuery = true)
     List<GetCategoriesWithTotalProducts> countProductByCategoryId();
 }
